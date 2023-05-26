bb648338d3b7d03fe9faadda29346b7fea01913b
HugoCardenasGon <HugoCardenasGon@gmail.com>
2023-05-26 16:33:00 -0600

    Add project files.

---
+++ ML/Coctel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Coctel
+    {
+        public int idDrink { get; set; }
+
+        public string? strDrink { get; set; }
+
+        public string? strDrinkThumb { get; set; }
+
+        public string? strInstructions { get; set; }
+
+        public string? strIngredient1 { get; set; }
+
+        public string? strIngredient2 { get; set; }
+
+        public string? strIngredient3 { get; set; }
+
+        public List<object>? Cocteles { get; set; }
+
+        public string? letra { get; set; }
+    }
+}
---
+++ PL/Controllers/CoctelController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ML;
+using Newtonsoft.Json;
+using System.Dynamic;
+
+namespace PL.Controllers
+{
+    public class CoctelController : Controller
+    {
+        [HttpGet]
+        public ActionResult GetAll(string letra = "a")
+        {
+            ML.Coctel coctel = new ML.Coctel();
+            coctel.Cocteles = new List<object>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
+
+                var responseTask = client.GetAsync("search.php?f=" + letra);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<dynamic>();
+                    readTask.Wait();
+                        
+                    foreach (var resultItem in readTask.Result.drinks)
+                    {
+                        ML.Coctel ResultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Coctel>(resultItem.ToString());
+                        coctel.Cocteles.Add(ResultItemList);
+                    }
+                }
+            }
+            return View(coctel);
+        }
+
+        [HttpPost]
+        public ActionResult GetAll(ML.Coctel coctel)
+        {
+            if (coctel.strDrink != null)
+            {
+                coctel.Cocteles = new List<object>();
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
+
+                    var responseTask = client.GetAsync("search.php?s=" + coctel.strDrink);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<dynamic>();
+                        readTask.Wait();
+
+                        foreach (var resultItem in readTask.Result.drinks)
+                        {
+                            ML.Coctel ResultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Coctel>(resultItem.ToString());
+                            coctel.Cocteles.Add(ResultItemList);
+                        }
+                    }
+                }
+                return View(coctel);
+            }
+            else
+            {
+                coctel.Cocteles = new List<object>();
+
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
+
+                    var responseTask = client.GetAsync("filter.php?i=" + coctel.strIngredient1);
+                    responseTask.Wait();
+
+                    var result = responseTask.Result;
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var readTask = result.Content.ReadAsAsync<dynamic>();
+                        readTask.Wait();
+
+                        foreach (var resultItem in readTask.Result.drinks)
+                        {
+                            ML.Coctel ResultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Coctel>(resultItem.ToString());
+
+                            using (var clientId = new HttpClient())
+                            {
+                                clientId.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
+
+                                var responseTaskId = clientId.GetAsync("lookup.php?i=" + ResultItemList.idDrink);
+                                responseTaskId.Wait();
+
+                                var resultId = responseTaskId.Result;
+
+                                if (resultId.IsSuccessStatusCode)
+                                {
+                                    var readTaskId = resultId.Content.ReadAsAsync<dynamic>();
+                                    readTaskId.Wait();
+
+                                    foreach (var resultItemId in readTaskId.Result.drinks)
+                                    {
+                                        ML.Coctel ResultItemListId = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Coctel>(resultItemId.ToString());
+                                        coctel.Cocteles.Add(ResultItemListId);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    return View(coctel);
+                }
+            }
+        }
+    }
+}
